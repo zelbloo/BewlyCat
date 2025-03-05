f81f3140a33b447c4230682b4319ddc74cf18232
keleus <38204020+keleus@users.noreply.github.com>
2025-03-05 13:16:04 +0800

    :lock: remove tabs permission,use activeTab

--- src/manifest.ts
+++ src/manifest.ts
@@ -38,7 +38,7 @@ export async function getManifest() {
     permissions: [
       'storage',
       'declarativeNetRequest',
-      'tabs',
+      'activeTab',
       ...isFirefox
         ? ['webRequest', 'webRequestBlocking', 'cookies']
         : [],
